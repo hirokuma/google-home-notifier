d1e4abb3356caade79cf12a48f75fb6b66f9bd4e
hirokuma <hiro9ma@gmail.com>
2019-06-15 14:28:04 +0900

    update

--- example2.js
+++ example2.js
@@ -1,9 +1,8 @@
 var googlehome = require('./google-home-notifier');
 
 googlehome.device('something', 'ja');
-googlehome.ip('192.168.x.xx');
-//googlehome.ip('xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx.local');
-googlehome.speed(0.24);
+googlehome.ip('xx.xx.xx.xx');
+googlehome.speed(1.9);
 googlehome.notify('おちゃづけ', function(res) {
   console.log(res);
 });
